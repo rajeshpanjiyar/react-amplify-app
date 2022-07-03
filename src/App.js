ad6bb3fea8278c9599574adf8f5ea8590455041a
Rajesh Panjiyar <rajeshpanjiyar12@gmail.com>
2022-07-03 10:47:49 +0530

    added more lines to app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ function App() {
   return (
     <>
     <h1>React Amplify App</h1>
+    <div className="card" >
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p  class="btn btn-primary">Go somewhere</p>
+  </div>
+</div>
     </>
   );
 }
